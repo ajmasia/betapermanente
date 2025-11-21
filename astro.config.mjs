13ca6ecc0a3b7dc4fab6523f72bd4632c970c41e
Antonio Masiá <ajmasia.dev@ysnp.link>
2025-11-21 20:59:00 +0100

    chore: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://betapermanente.dev',
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
